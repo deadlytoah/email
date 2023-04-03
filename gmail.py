1d4d898408d5cd835581f033639d7147f8970ea0
Hee Suk Shin <heesuk@gmail.com>
2023-04-03 11:16:45 +0900

    Implement check command

--- gmail.py
+++ gmail.py
@@ -17,6 +17,14 @@ SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 CLIENT_SECRETS_FILE = os.path.expanduser('~/.credentials/gmail.json')
 TOKEN_FILE = os.path.expanduser('~/.credentials/gmail-token.json')
 
+EMAIL_ADDRESS = "thevoicekorea+chat@gmail.com"
+
+gmail = None
+
+class NoMessagesException(Exception):
+    def __init__(self):
+        super().__init__("no messages")
+
 class State(Enum):
     SENDING = 0
     RECEIVING = 1
@@ -25,44 +33,52 @@ class StateException(Exception):
     def __init__(self, state):
         self.state = state
 
-def gmail_main():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists(TOKEN_FILE):
-        creds = Credentials.from_authorized_user_file(TOKEN_FILE, SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                CLIENT_SECRETS_FILE, SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open(TOKEN_FILE, 'w') as token:
-            token.write(creds.to_json())
+class Gmail:
+    def authenticate(self):
+        creds = None
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists(TOKEN_FILE):
+            creds = Credentials.from_authorized_user_file(TOKEN_FILE, SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    CLIENT_SECRETS_FILE, SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open(TOKEN_FILE, 'w') as token:
+                token.write(creds.to_json())
 
-    try:
-        # Call the Gmail API
-        service = build('gmail', 'v1', credentials=creds)
-        results = service.users().labels().list(userId='me').execute()
-        labels = results.get('labels', [])
+        try:
+            # Call the Gmail API
+            self.service = build('gmail', 'v1', credentials=creds)
+            return self
+        except HttpError as error:
+            raise GmailException(error)
 
-        if not labels:
-            print('No labels found.')
-            return
-        print('Labels:')
-        for label in labels:
-            print(label['name'])
+    def check(self, mailto):
+        try:
+            # Call the Gmail API to search for messages addressed to "mailto"
+            query = "to:" + mailto
+            results = self.service.users().messages().list(userId='me', q=query).execute()
+            messages = results.get('messages', [])
+            # Retrieve the message details for each matching message
+            for message in messages:
+                msg = self.service.users().messages().get(userId='me', id=message['id']).execute()
+                # Process the message as needed
+            return messages
+        except HttpError as error:
+            print("print error", error)
+            print("print str(error)", str(error))
+            raise GmailException(error)
 
-    except HttpError as error:
-        # TODO(developer) - Handle errors from gmail API.
-        print(f'An error occurred: {error}')
+class GmailException(Exception):
+    def __init__(self, inner):
+        super().__init__(inner)
 
 def ok(socket, array):
     socket.send_multipart([b"OK"] + [arg.encode() for arg in array])
@@ -71,13 +87,25 @@ def error(socket, message):
     socket.send_multipart([b"ERROR", message])
 
 def list_commands():
-    return list(COMMAND_MAP.keys())
+    return list(command_map().keys())
 
-COMMAND_MAP = {
-    "help": list_commands,
-}
+def check():
+    messages = gmail.check(mailto=EMAIL_ADDRESS)
+    if messages:
+        return messages
+    else:
+        raise NoMessagesException()
+
+def command_map():
+    return {
+        "check": check,
+        "help": list_commands,
+    }
 
 def main():
+    global gmail
+    gmail = Gmail().authenticate()
+
     context = zmq.Context()
 
     # Create a socket for the server
@@ -105,8 +133,8 @@ def main():
             print("received command", command, file=sys.stderr)
 
             # Process the request
-            if command in COMMAND_MAP:
-                response = COMMAND_MAP[command]()
+            if command in command_map():
+                response = command_map()[command]()
 
                 # Send the response back to the client
                 if state == State.SENDING:
